5df8806586d0431d189d884891803496228cea69
madiv9820 <themathur901@gmail.com>
2024-12-31 13:09:14 +0530

    Unit Test Cases for the problem

---
+++ UnitTest.cpp
@@ -0,0 +1,35 @@
+#include "Solution.hpp"
+#include <iostream>
+using namespace std;
+
+struct testcase {
+    vector<int> days;
+    vector<int> costs;
+    int output;
+};
+
+class UnitTest {
+private:
+    Solution obj;
+    vector<testcase> testCases;
+public:
+    UnitTest() {
+        testCases = {{{1,4,6,7,8,20}, {2,7,15}, 11}, 
+                     {{1,2,3,4,5,6,7,8,9,10,30,31}, {2,7,15}, 17},
+                     {{1,4,6,7,8,20}, {7,2,15}, 6}};
+    }
+
+    void test() {
+        for(int index = 0; index < testCases.size(); ++index) {
+            vector<int> days = testCases[index].days, costs = testCases[index].costs;
+            int output = testCases[index].output;
+            int result = obj.mincostTickets(days, costs);
+            cout << "TestCase " << index+1 << ": " << ((result == output) ? "passed":"failed") << endl;
+        }
+    }
+};
+
+int main() {
+    UnitTest test;
+    test.test();
+}
--- UnitTest.py
+++ UnitTest.py
@@ -5,7 +5,8 @@ from Solution import Solution
 class UnitTest(unittest.TestCase):
     def setUp(self):
         self.__testCases = {1: ([1,4,6,7,8,20], [2,7,15], 11), 
-                            2: ([1,2,3,4,5,6,7,8,9,10,30,31], [2,7,15], 17)}
+                            2: ([1,2,3,4,5,6,7,8,9,10,30,31], [2,7,15], 17),
+                            3: ([1,4,6,7,8,20], [7,2,15], 6)}
         self.__obj = Solution()
         return super().setUp()
     
@@ -19,6 +20,13 @@ class UnitTest(unittest.TestCase):
     @timeout(0.5)
     def test_case_2(self):
         days, costs, output = self.__testCases[2]
+        result = self.__obj.mincostTickets(days, costs)
+        self.assertIsInstance(result, int)
+        self.assertEqual(result, output)
+
+    @timeout(0.5)
+    def test_case_3(self):
+        days, costs, output = self.__testCases[3]
         result = self.__obj.mincostTickets(days, costs)
         self.assertIsInstance(result, int)
         self.assertEqual(result, output)
